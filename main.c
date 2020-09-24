5c7a8c9076857e4d6a94eb102bc3e4a5dc1283bf
Hiurt <69557622+Hiurt@users.noreply.github.com>
2020-09-23 23:38:42 -0300

    Create main.c
    Função principal.

---
+++ main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "funcoes.c"
+#include "ia.c"
+
+int main() {
+	int aux;
+	char jogadordavez, vencedor, primeirojogador = 'o';
+	
+	srand(time(NULL));
+	
+	for(;;) {
+		jogadordavez = trocarjogador(primeirojogador);
+		primeirojogador = trocarjogador(primeirojogador);
+		vencedor = '0';
+		
+		inicializar();
+		int cheio, posicao;
+		
+		while(vencedor == '0') {
+			imprimir(tabuleiro);
+			
+			if(jogadordavez == 'x') {
+				posicao = lerterminal(jogadordavez);
+			}
+			else if(jogadordavez == 'o') {
+				printf("o? ...\n");
+				usleep(500000);
+				posicao = ia('o');
+				//getchar();
+			}
+			
+			jogar(jogadordavez, posicao);
+			
+			vencedor = checarvencedor(tabuleiro, jogadordavez);
+			
+			if(vencedor != '0') {
+				break;
+			}
+			
+			cheio = 1;
+			for(aux = 0; aux < 9; aux++) {
+				if(tabuleiro[aux] == ' ') {
+					cheio = 0;
+					break;
+				}
+			}
+			if(cheio == 1) {
+				vencedor = 'n';
+				break;
+			}
+			
+			jogadordavez = trocarjogador(jogadordavez);
+		}
+		
+		imprimir(tabuleiro);
+		
+		if(vencedor == 'n') {
+			printf("\nEmpate!");
+		}
+		else {
+			printf("%c venceu a partida.\n", vencedor);
+		}
+		
+		switch(vencedor) {
+			case 'x':
+				winsx++;
+				break;
+				
+			case 'o':
+				winso++;
+				break;
+			
+			case 'n':
+				empates++;
+				break;
+		}
+		
+		getchar();
+	}
+}
