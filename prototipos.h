054417c1f7112f362d1aa7fdebcc295e5e8abc1a
Hiurt <69557622+Hiurt@users.noreply.github.com>
2020-09-24 16:28:56 -0300

    Delete prototipos.h

--- prototipos.h
+++
@@ -1,9 +0,0 @@
-void inicializar();
-void imprimir(char tab[9]);
-char trocarjogador(char atual);
-char checarvencedor(char tab[9], char jogadordavez);
-int jogar(char jog, int pos);
-int lerterminal(char jogadordavez);
-
-int ia(char maquina);
-int prever(char maq);
