f6a1e99d8a48903e18206c77ee5866e9225c3149
Hiurt <69557622+Hiurt@users.noreply.github.com>
2020-09-23 23:29:47 -0300

    Create prototipos.h

---
+++ prototipos.h
@@ -0,0 +1,9 @@
+void inicializar();
+void imprimir(char tab[9]);
+char trocarjogador(char atual);
+char checarvencedor(char tab[9], char jogadordavez);
+int jogar(char jog, int pos);
+int lerterminal(char jogadordavez);
+
+int ia(char maquina);
+int prever(char maq);
